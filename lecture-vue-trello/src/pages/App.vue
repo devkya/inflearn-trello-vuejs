f01b27192ed36aa3689621863cfbe89acc241941
devKya <kihaok@gmail.com>
2022-12-20 18:03:33 +0900

    Setting Pages

---
+++ lecture-vue-trello/src/pages/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   Start Code
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
--- lecture-vue-trello/vue.config.js
+++ lecture-vue-trello/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+        entry: 'src/pages/main.js', // entry for the page
+        template: 'public/index.html', // output as dist
+        // when using title option,
+        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+        title: 'Index Page',
+        // chunks to include on this page, by default includes
+        // extracted common chunks and vendor chunks.
+    }
+}
 })
